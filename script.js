e3a681c19ebedf08277d9555a830049fb3ab4cd1
SRK SHADHIN <mt6w6v6x5b@privaterelay.appleid.com>
2026-03-06 23:51:55 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
+
 const card = document.getElementById("card");
+const loginBtn = document.getElementById("loginBtn");
+const backBtn = document.getElementById("backBtn");
+
+loginBtn.onclick = () => {
+card.classList.add("active");
+}
+
+backBtn.onclick = () => {
+card.classList.remove("active");
+}
+
+/* desktop tilt */
 
 document.addEventListener("mousemove",(e)=>{
 
-const x = (window.innerWidth/2 - e.pageX) / 25;
-const y = (window.innerHeight/2 - e.pageY) / 25;
+if(window.innerWidth < 650) return;
 
-card.style.transform = `skewX(-8deg) rotateY(${x}deg) rotateX(${y}deg)`;
+const x = (window.innerWidth/2 - e.pageX) / 35;
+const y = (window.innerHeight/2 - e.pageY) / 35;
+
+card.style.transform =
+`skewX(-8deg) rotateY(${x}deg) rotateX(${y}deg)`;
 
 });
 
 document.addEventListener("mouseleave",()=>{
-card.style.transform="skewX(-8deg) rotateY(0deg) rotateX(0deg)";
+
+card.style.transform="skewX(-8deg) rotateY(0) rotateX(5deg)";
+
+});
+
+/* mobile touch tilt */
+
+card.addEventListener("touchmove",(e)=>{
+
+const t = e.touches[0];
+
+const x = (window.innerWidth/2 - t.pageX) / 40;
+const y = (window.innerHeight/2 - t.pageY) / 40;
+
+card.style.transform =
+`rotateY(${x}deg) rotateX(${y}deg)`;
+
 });
